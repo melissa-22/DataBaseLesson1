ec81a2728c06b8e2fcb3e4418d1625d693ebd32f
melissa-22 <115267573+melissa-22@users.noreply.github.com>
2022-10-08 13:31:09 +0300

    v1.0
    Готовая форма для связи с БД ACCESS.
    МДК.01.04

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(649, 317);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(695, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(307, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить запись";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(830, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 38);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(783, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(690, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Категория";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(830, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 38);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(690, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Скидка, %";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(830, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 38);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(695, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(307, 47);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Удалить запись";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(695, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(307, 47);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Обновить запись";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 366);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string _connectionString =
+            @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|Worker.mdb;";
+        private OleDbConnection _connection;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            DataGridViewDbLoad("tableName");
+        }
+
+
+        /// <summary>
+        /// Пример вывода таблицы в DataGridView
+        /// </summary>
+        /// <param name="tableName">Название таблицы</param>
+        private void DataGridViewDbLoad(string tableName)
+        {
+            _connection = new OleDbConnection(_connectionString);
+
+            _connection.Open();
+
+            // виртуальная таблица
+            DataTable table = new DataTable();
+
+            // SQL-запрос
+            string command = $"SELECT * FROM [{tableName}]";
+
+            // команда для базы данных
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command, _connection);
+           
+            // заполнение виртуальной таблицы
+            adapter.Fill(table);
+
+            // передача виртуальной таблицы в качестве источника данных
+            dataGridView1.DataSource = table;
+
+            _connection.Close();
+        }
+
+        private void button1_Click()
+        {
+            
+        }
+
+        /// <summary>
+        /// Метод добавляет новую запись в таблицу Категории
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(textBox1.Text);
+            string type = textBox2.Text;
+            int discount = Convert.ToInt32(textBox3.Text);
+
+            // запрос
+            string query = $"INSERT INTO [tableName] " +
+                $"([ID], [Категория товара], [Скидка %]) " +
+                $"VALUES ({id}, '{type}', {discount})";
+
+            _connection = new OleDbConnection(_connectionString);
+
+            _connection.Open();
+
+            OleDbCommand command = new OleDbCommand(query, _connection);
+
+            // запрос, который не выводит ничего, а только выполняет команду
+            command.ExecuteNonQuery();
+
+            _connection.Close();
+            DataGridViewDbLoad("tableName");
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(textBox1.Text);
+            string type = textBox2.Text;
+            int discount = Convert.ToInt32(textBox3.Text);
+
+            // запрос
+            string query = $"UPDATE [tableName] SET [Категория товара] = '{type}', " +
+                $"[Скидка %] = {discount} " +
+                $"WHERE [ID] = {id}";
+
+            _connection = new OleDbConnection(_connectionString);
+
+            _connection.Open();
+
+            OleDbCommand command = new OleDbCommand(query, _connection);
+
+            // запрос, который не выводит ничего, а только выполняет команду
+            command.ExecuteNonQuery();
+
+            _connection.Close();
+            DataGridViewDbLoad("tableName");
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(textBox1.Text);            
+
+            // запрос
+            string query = $"DELETE FROM [tableName] " +
+                $"WHERE [ID] = {id}";
+
+            _connection = new OleDbConnection(_connectionString);
+
+            _connection.Open();
+
+            OleDbCommand command = new OleDbCommand(query, _connection);
+
+            // запрос, который не выводит ничего, а только выполняет команду
+            command.ExecuteNonQuery();
+
+            _connection.Close();
+            DataGridViewDbLoad("tableName");
+        }
+    }
+}
